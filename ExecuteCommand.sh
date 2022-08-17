626f02490c0f2d02bc728d9e30ac2a16ce5f2437
akasati02 <47314190+akasati02@users.noreply.github.com>
2022-08-17 19:53:31 +0530

    Update ExecuteCommand.sh

--- ExecuteCommand.sh
+++ ExecuteCommand.sh
@@ -1,5 +1,5 @@
 echo "Inside ExecuteCommand"
 
-\TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "TotalTestConfiguration"
+./TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "TotalTestConfiguration"
 
 #TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "\TotalTestConfiguration"
