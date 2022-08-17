d4e17047b7b6964f79e8cea392c5e8ca6f1278ad
akasati02 <47314190+akasati02@users.noreply.github.com>
2022-08-17 13:26:01 +0530

    Update ExecuteCommand.bat

--- ExecuteCommand.bat
+++ ExecuteCommand.bat
@@ -1,1 +1,1 @@
-TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "\TotalTestConfiguration"
+TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "TotalTestConfiguration"
