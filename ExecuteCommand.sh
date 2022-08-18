a6750c9c5a02a078c1d33f0dbce77cdedac2d955
akasati02 <47314190+akasati02@users.noreply.github.com>
2022-08-18 19:01:47 +0530

    Update ExecuteCommand.sh

--- ExecuteCommand.sh
+++ ExecuteCommand.sh
@@ -4,4 +4,4 @@ echo "Inside ExecuteCommand"
 
 #echo TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "C:\\Users\\aasati\\git\\Topaz_Workbench_CLI_Test\\CLI_CWKTCOBX\\Tests\\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "./TotalTestConfiguration"
 
-./TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX/Tests/" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "TotalTestConfiguration"
+TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX/Tests/" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "TotalTestConfiguration"
