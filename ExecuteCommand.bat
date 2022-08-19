ff3949841259f6c5ffa68e4a8cd6a744a30b26ab
akasati02 <47314190+akasati02@users.noreply.github.com>
2022-08-19 16:53:44 +0530

    Update ExecuteCommand.bat

--- ExecuteCommand.bat
+++ ExecuteCommand.bat
@@ -1,5 +1,5 @@
 echo "Inside ExecuteCommand"
 
-TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "C:\actions-runner\_work\Topaz_Workbench_CLI_Test\Topaz_Workbench_CLI_Test\CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "C:\actions-runner\_work\Topaz_Workbench_CLI_Test\Topaz_Workbench_CLI_Test\TotalTestConfiguration"
+TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "C:\Users\aasati\Compuware\Workbench\workspace\CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "C:\Users\aasati\Compuware\Workbench\workspace\TotalTestConfiguration"
 
 #TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "\TotalTestConfiguration"
