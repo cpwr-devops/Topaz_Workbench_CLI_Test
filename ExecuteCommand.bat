c4cc23482a90096b15e4369c275a9f2658a6c66e
akasati02 <47314190+akasati02@users.noreply.github.com>
2022-08-22 17:15:59 +0530

    Update ExecuteCommand.bat

--- ExecuteCommand.bat
+++ ExecuteCommand.bat
@@ -7,6 +7,6 @@ set configration="C:\actions-runner\_work\Topaz_Workbench_CLI_Test\Topaz_Workben
 
 rem call .\TotalTestFTCLI.bat -e %hciConnection% ^ %-passticket% ^ -f %testLocationPath% ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir %configration%
 
-call TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "C:\\actions-runner\\_work\Topaz_Workbench_CLI_Test\\Topaz_Workbench_CLI_Test\\CLI_CWKTCOBX\\Tests\\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "C:\\actions-runner\\_work\\Topaz_Workbench_CLI_Test\\Topaz_Workbench_CLI_Test\\TotalTestConfiguration"
+call TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "C:\\actions-runner\\_work\\Topaz_Workbench_CLI_Test\\Topaz_Workbench_CLI_Test\\CLI_CWKTCOBX\\Tests\\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "C:\\actions-runner\\_work\\Topaz_Workbench_CLI_Test\\Topaz_Workbench_CLI_Test\\TotalTestConfiguration"
 
 rem TotalTestFTCLI.bat -e "10.100.4.82:16196" ^ -passticket ^ -f "CLI_CWKTCOBX\Tests\" ^ -R -g TTTReport -G -v 6 -l CLI ^ -log DEBUG ^ -cfgdir "\TotalTestConfiguration"
