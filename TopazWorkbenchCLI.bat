246dff9bc7e9425d5c832ae805537850f99ccda6
akasati02 <47314190+akasati02@users.noreply.github.com>
2022-08-22 16:27:43 +0530

    Update TopazWorkbenchCLI.bat

--- TopazWorkbenchCLI.bat
+++ TopazWorkbenchCLI.bat
@@ -134,7 +134,7 @@ rem Execute the CLI
 :okJava
 if not defined WS_LOC goto noWorkspace
 echo Using customized WORKSPACE: %WS_LOC%
-rem rmdir /s /q "%WS_LOC%"
+rmdir /s /q "%WS_LOC%"
 mkdir "%WS_LOC%"
 mkdir "%WS_LOC%\configuration"
 copy %LOGBACK_LOC% "%WS_LOC%\configuration\logback.xml"
@@ -147,8 +147,8 @@ exit /b %errorlevel%
 :noWorkspace
 echo No WORKSPACE provided, using default WORKSPACE: %CLI_LOC%%DEFAULT_WORKSPACE%
 rem rmdir /s /q "%CLI_LOC%%DEFAULT_WORKSPACE%
-mkdir "%CLI_LOC%%DEFAULT_WORKSPACE%"
-mkdir "%CLI_LOC%%DEFAULT_WORKSPACE%\configuration"
+rem mkdir "%CLI_LOC%%DEFAULT_WORKSPACE%"
+rem mkdir "%CLI_LOC%%DEFAULT_WORKSPACE%\configuration"
 copy %LOGBACK_LOC% "%CLI_LOC%%DEFAULT_WORKSPACE%\configuration\logback.xml"
 %_RUNJAVA% -jar "%CLI_LOC%plugins\org.eclipse.equinox.launcher_1.5.0.v20180512-1130.jar" ^
 -configuration "%CLI_LOC%configuration" ^
